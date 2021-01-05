60efcf43cd2bff9a08472f96b0fa70f877901228
Eduardo Vicente <eduardo.vicente@inetsys.es>
2021-01-05 17:02:02 +0100

    Simple date selector; demo file

---
+++ src/components/Panel.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="sdp-datepicker--panel--body">
+        <button type="button"
+            v-for="(item, index) of items"
+            :key="index"
+            :class="{selected: item.selected, dimmed: item.dimmed, weekend: item.weekend }"
+            @click="select(item.value)"
+        >{{ item.label }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    emits: ['select'],
+    props: {
+        items: {
+            type: Array,
+            default() {
+                return []
+            },
+        },
+    },
+    methods: {
+        select(value) {
+            this.$emit('select', value)
+        },
+    },
+}
+</script>
